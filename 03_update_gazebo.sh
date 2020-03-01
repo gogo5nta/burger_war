dde8f64842578e056d9000223a700ed714f693bf
Tomohide Ishigami <gogo5nta@gmail.com>
2020-03-02 00:29:01 +0900

    Update 03_update_gazebo.sh

--- 03_update_gazebo.sh
+++ 03_update_gazebo.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+#ubuntu 16.04でインストールしたROS(kinetic)はgazeboに不具合があるため、ver7にアップデートすることをお勧め(7.15までアップデート可能)
+#https://roboticshubchallenge.slack.com/archives/CL9TFG2RZ/p1564533784053800?thread_ts=1564533261.050300&cid=CL9TFG2RZ
+
+sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable lsb_release -cs main" > /etc/apt/sources.list.d/gazebo-stable.list'
+wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
+sudo apt-get update
